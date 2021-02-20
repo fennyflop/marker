32add6ff5514ae771e8f3e5915fcf66731ec402b
Alex Semochkin <fennyflop@gmail.com>
2021-02-20 21:38:25 +0300

    registration block added

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -2,12 +2,16 @@ import './Registration.css';
 
 import registrationIcon from '../../images/avatar.png';
 import padlockIcon from '../../images/padlock.png';
+import checkMark from '../../images/checkmark.png';
+import errorIcon from '../../images/error.png';
+import peak from '../../images/peak.png';
 import { useEffect, useState } from 'react';
 
 function Registration() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordState, setPasswordState] = useState(true);
     const [isEmailValid, setIsEmailValid] = useState(false);
     const [isPasswordValid, setIsPasswordValid] = useState(false);
 
@@ -20,6 +24,10 @@ function Registration() {
         setPassword(evt.target.value);
         setIsPasswordValid(evt.target.value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/) ? true : false);
     };
+
+    function peakPassword() {
+        setPasswordState(!passwordState);
+    }
 
     useEffect(() => {
         console.log(email, password);
@@ -35,12 +43,20 @@ function Registration() {
                 <div className="registration__item">
                     <img className="registration__icon" src={registrationIcon} alt="icon" />
                     <input className="registration__input" placeholder="Электронная почта" type="email" required onChange={handleEmail} />
+                    <div className="registration__side">
+                        <img className="registration__tool" src={isEmailValid ? checkMark : errorIcon} alt="success" />
+                    </div>
                 </div>
                 <div className="registration__item">
                     <img className="registration__icon" src={padlockIcon} alt="icon" />
-                    <input className="registration__input" placeholder="Пароль" type="password" required onChange={handlePassword} />
+                    <input className="registration__input" placeholder="Пароль" type={passwordState ? 'password' : 'text'} required onChange={handlePassword} />
+                    <div className="registration__side">
+                        <img className="registration__tool" src={peak} alt="peak" onClick={peakPassword} />
+                        <img className="registration__tool" src={isPasswordValid ? checkMark : errorIcon} alt="success" />
+                    </div>
                 </div>
-                <button className="registration__submit" type="submit">Зарегистрироваться</button>
+                <p className={`registration__advice ${isPasswordValid ? 'registration__advice-hidden' : ''}`}></p>
+                <button className="registration__submit" type="submit" disabled={!(isPasswordValid && isEmailValid)}>Зарегистрироваться</button>
                 <p className="registration__subtext">Есть аккаунт? Вход</p>
             </form>
         </section>
